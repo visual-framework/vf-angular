0514b96577bfea33f4f5d4680491bdca11e2be5e
Bhushan Palsapure <bhushanp@ebi.ac.uk>
2026-07-16 13:25:44 +0100

    bug: Issue-2367 - Fixed the search functionality

---
+++ src/app/components/search/search-index.ts
@@ -0,0 +1,51 @@
+export interface SearchPage {
+  id: string;
+  title: string;
+  text: string;
+  url: string;
+}
+
+export const COMPONENT_SEARCH_INDEX: SearchPage[] = [
+  {
+    id: '0',
+    title: 'vf-banner',
+    text: 'vf-banner',
+    url: '/components/vf-banner'
+  },
+  {
+    id: '1',
+    title: 'vf-hero',
+    text: 'vf-hero',
+    url: '/components/vf-hero'
+  },
+  {
+    id: '2',
+    title: 'vf-back-to-top',
+    text: 'vf-back-to-top',
+    url: '/components/vf-back-to-top'
+  },
+  {
+    id: '3',
+    title: 'vf-tabs',
+    text: 'vf-tabs',
+    url: '/components/vf-tabs'
+  },
+  {
+    id: '4',
+    title: 'vf-badge',
+    text: 'vf-badge',
+    url: '/components/vf-badge'
+  },
+  {
+    id: '5',
+    title: 'vf-blockquote',
+    text: 'vf-blockquote',
+    url: '/components/vf-blockquote'
+  },
+  {
+    id: '6',
+    title: 'vf-button',
+    text: 'vf-button',
+    url: '/components/vf-button'
+  }
+];
